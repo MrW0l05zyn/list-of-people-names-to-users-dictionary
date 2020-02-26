5c08012ff8701153045dc37a8f1a65db395e1418
marcelowoloszyndiaz <marcelo@wolcas.cl>
2020-02-26 01:36:03 -0300

    Configuración de argumentos de linea de comandos.

--- ListPeopleNamesToUsersDictionary.py
+++ ListPeopleNamesToUsersDictionary.py
@@ -1,4 +1,14 @@
-import sys
+import argparse
 
-print (sys.argv[0]) # Nombre de archivo.
-print (sys.argv[1]) # Archivo de listado de nombres de personas.
+# configuración de argumentos de linea de comandos
+parser = argparse.ArgumentParser(
+    usage='%(prog)s <list>',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='Example: ./%(prog)s list-people-names.txt',
+    epilog=''
+)
+parser.add_argument('<list>', action='store', help='list of people names')
+parser.add_argument('-v', '--version', action='version', version='Version: 0.1')
+
+# lectura de argumentos desde linea de comandos
+parser.parse_args()
