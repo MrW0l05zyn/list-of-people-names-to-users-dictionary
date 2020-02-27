c6f94332dc8f57ed7c5c538cc143fe468089dcfe
marcelowoloszyndiaz <marcelo@wolcas.cl>
2020-02-26 23:01:39 -0300

    Update ListPeopleNamesToUsersDictionary.py

--- ListPeopleNamesToUsersDictionary.py
+++ ListPeopleNamesToUsersDictionary.py
@@ -1,14 +1,61 @@
 import argparse
 
+# variables
+usersDictionary = []
+
 # configuración de argumentos de linea de comandos
-parser = argparse.ArgumentParser(
+analizador = argparse.ArgumentParser(
     usage='%(prog)s <list>',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description='Example: ./%(prog)s list-people-names.txt',
     epilog=''
 )
-parser.add_argument('<list>', action='store', help='list of people names')
-parser.add_argument('-v', '--version', action='version', version='Version: 0.1')
+analizador.add_argument('list', action='store', help='list of people names', type=argparse.FileType('r'))
+analizador.add_argument('-v', '--version', action='version', version='Version: 0.1')
 
 # lectura de argumentos desde linea de comandos
-parser.parse_args()
+argumentos = analizador.parse_args()
+archivo = argumentos.list
+
+# lectura de lista de nombres de personas
+for personas in archivo.readlines():
+    persona = personas.lower().split()
+    nombre = persona[0]         #elliot
+    apellido = persona[1]       #alderson
+
+    usersDictionary.append(nombre + apellido)           #elliotalderson
+    usersDictionary.append(apellido + nombre)           #aldersonelliot
+
+    usersDictionary.append(nombre + '.' + apellido)     #elliot.alderson
+    usersDictionary.append(apellido + '.' + nombre)     #alderson.elliot
+    usersDictionary.append(nombre + '_' + apellido)     #elliot_alderson
+    usersDictionary.append(apellido + '_' + nombre)     #alderson_elliot
+    usersDictionary.append(nombre + '-' + apellido)     #elliot-alderson
+    usersDictionary.append(apellido + '-' + nombre)     #alderson-elliot
+
+    usersDictionary.append(nombre + apellido[0])        #elliota
+    usersDictionary.append(apellido + nombre[0])        #aldersone
+    usersDictionary.append(nombre[0] + apellido)        #ealderson
+    usersDictionary.append(apellido[0] + nombre)        #aelliot
+
+    usersDictionary.append(nombre + '.' + apellido[0])  #elliot.a
+    usersDictionary.append(apellido + '.' + nombre[0])  #alderson.e
+    usersDictionary.append(nombre + '_' + apellido[0])  #elliot_a
+    usersDictionary.append(apellido + '_' + nombre[0])  #alderson_e
+    usersDictionary.append(nombre + '-' + apellido[0])  #elliot-a
+    usersDictionary.append(apellido + '-' + nombre[0])  #alderson-e
+
+    usersDictionary.append(nombre[0] + '.' + apellido)  #e.alderson
+    usersDictionary.append(apellido[0] + '.' + nombre)  #a.elliot
+    usersDictionary.append(nombre[0] + '_' + apellido)  #e_alderson
+    usersDictionary.append(apellido[0] + '_' + nombre)  #a_elliot
+    usersDictionary.append(nombre[0] + '-' + apellido)  #e-alderson
+    usersDictionary.append(apellido[0] + '-' + nombre)  #a-elliot
+
+    usersDictionary.append(nombre)                      #elliot
+    usersDictionary.append(apellido)                    #alderson
+archivo.close()
+
+# impresión de usernames
+for userName in usersDictionary:
+    print(userName)
