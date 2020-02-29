d5157fae43f7de9b8654885eb3e88650f0037155
marcelowoloszyndiaz <marcelo@wolcas.cl>
2020-02-29 03:59:41 -0300

    Incorporación de opción -n/--name

--- ListPeopleNamesToUsersDictionary.py
+++ ListPeopleNamesToUsersDictionary.py
@@ -1,32 +1,35 @@
 import argparse
+import sys
 
 # variables
 usersDictionary = []
 
 # configuración de analizador
 analizador = argparse.ArgumentParser(
-    usage='%(prog)s {list} [-o file]',
+    usage='%(prog)s [-n name] [-l list] [-o file]',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description="""examples:
 
-    ./%(prog)s list-people-names.txt
-    ./%(prog)s list-people-names.txt -o users-dictionary.txt""",
+    ./%(prog)s -n 'Elliot Alderson'
+    ./%(prog)s -l list-people-names.txt
+    ./%(prog)s -n 'Elliot Alderson' -o users-dictionary.txt
+    ./%(prog)s -l list-people-names.txt -o users-dictionary.txt""",
     epilog=''
 )
-analizador.add_argument('list', action='store', help='list of people names', type=argparse.FileType('r'))
-analizador.add_argument('-o', action='store', help='output users dictionary', dest='file')
-analizador.add_argument('-V', '--version', action='version', version='Version: 0.1')
+# argumentos de analizador
+analizador.add_argument('-n', '--name', action='store', help='name of person, example: \'Elliot Alderson\' (first name and last name)', dest='name', type=str)
+analizador.add_argument('-l', '--list', action='store', help='list of people names, example: list-people-names.txt', dest='list', type=argparse.FileType('r'))
+analizador.add_argument('-o', '--output', action='store', help='output users dictionary, example: users-dictionary.txt', dest='file', type=str)
+analizador.add_argument('-V', '--version', action='version', version='Version: 0.2')
 
 # lectura de argumentos desde linea de comandos
 argumentos = analizador.parse_args()
-archivo = argumentos.list
 
-# lectura de lista de nombres de personas
-for personas in archivo.readlines():
-    persona = personas.lower().split()
-    nombre = persona[0]         #elliot
-    apellido = persona[1]       #alderson
+# validación de ingreso de parámetros --name o --list
+if not argumentos.name and not argumentos.list:
+   analizador.print_help()
 
+def ListPeopleNamesToUsersDictionary(nombre, apellido):
     usersDictionary.append(nombre)                      #elliot
     usersDictionary.append(apellido)                    #alderson
 
@@ -58,7 +61,29 @@ for personas in archivo.readlines():
     usersDictionary.append(apellido[0] + '_' + nombre)  #a_elliot
     usersDictionary.append(nombre[0] + '-' + apellido)  #e-alderson
     usersDictionary.append(apellido[0] + '-' + nombre)  #a-elliot
-archivo.close()
+
+# argumento -n --name
+if argumentos.name:
+    persona = argumentos.name.lower().split()
+    # validación de nombre y apellido
+    if len(persona) == 2:
+        ListPeopleNamesToUsersDictionary(persona[0], persona[1])
+    else:
+        print('Format error: [-n/--name] Name of person, example: \'Elliot Alderson\' (first name and last name).')
+        sys.exit()
+
+# argumento -l --list
+if argumentos.list:
+    archivo = argumentos.list
+    for personas in archivo.readlines():
+        persona = personas.lower().split()
+        # validación de nombre y apellido
+        if len(persona) == 2:
+            ListPeopleNamesToUsersDictionary(persona[0], persona[1])
+        else:
+            print('Format error: [-n/--name] Name of person, example: \'Elliot Alderson\' (first name and last name).')
+            sys.exit()
+    archivo.close()
 
 # guarda diccionario de usuario en archivo
 if argumentos.file:
